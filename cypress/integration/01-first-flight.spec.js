e7fa4b68c6d69b700bb051e020ca10c4262ac786
Will Lorand <wlorand@cisco.com>
2022-07-10 15:51:06 -0700

    feat: added dashboard config and a first test test

--- cypress/integration/01-first-flight.spec.js
+++ cypress/integration/01-first-flight.spec.js
@@ -1,3 +1,21 @@
 /// <reference types="cypress" />
 
-describe('Create a New Item', () => {});
+describe('Create a New Item', () => {
+  // visit a page in a beforeEach
+  beforeEach(() => {
+    cy.visit('/jetsetter');
+  });
+  // write a test in an it() block
+  it('should have a form', () => {
+    cy.get('formm').should('not.exist');
+  });
+
+  //   it('should have the word "Tooth"'),
+  //     () => {
+  //       cy.contains('Tooth'); // syntax issue?
+  //     };
+
+  it('should type into an input field', () => {
+    cy.get('[data-test="new-item-input"]').type('Good Attitude');
+  });
+});
