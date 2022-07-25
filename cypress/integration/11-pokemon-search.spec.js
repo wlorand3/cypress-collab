b134fc57e4242d8dcb276607af3a26e0b3441964
Will Lorand <wlorand@cisco.com>
2022-07-24 17:12:18 -0700

    feat: intercept practice with pokemon api

--- cypress/integration/11-pokemon-search.spec.js
+++ cypress/integration/11-pokemon-search.spec.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 
+// some mock data - quick and dirty (better: make a fixture file)
 const pokemon = [
   { id: 1, name: 'Bumblesaur' },
   { id: 2, name: 'Charmer' },
@@ -9,28 +10,111 @@ const pokemon = [
 describe('Pokémon Search', () => {
   beforeEach(() => {
     cy.visit('/pokemon-search');
-
+    // aliases
     cy.get('[data-test="search"]').as('search');
     cy.get('[data-test="search-label"]').as('label');
-
+    // pattern: do intercept alias if used in multiple tests
     cy.intercept('/pokemon-search/api?*').as('api');
   });
 
-  it('should call the API when the user types', () => {});
+  // Dev Note: these seem to be step-by-step tests - small focused tasks - for learning, not real-world
+  it('1- should call the API when the user types', () => {
+    cy.get('@search').type('ivy');
+    // just test if API is called
+    cy.wait('@api');
+  });
 
-  it('should update the query parameter', () => {});
+  it('2- should update the query parameter', () => {
+    cy.get('@search').type('ivy');
+    cy.wait('@api');
+    // reminder de two args for .should() is common
+    // know you can also use .contains() here
+    cy.location('search').should('equal', '?name=ivy');
+  });
 
-  it('should call the API with correct query parameter', () => {});
+  // tests if did fire the right API request
+  it('3- should call the API with correct query parameter', () => {
+    cy.get('@search').type('ivy');
+    // a- test a bit of the request
 
-  it('should pre-populate the search field with the query parameter', () => {});
+    // two ways demoed to do this:
+    // alt: expect(interception.request.url).to.contain('name=char');
+    // cy.wait('@api').its('request.url').should('contain', 'name=ivy');
+    // b- test a bit of the response
+  });
 
-  it('should render the results to the page', () => {});
+  it('4- should pre-populate the search field with the query parameter', () => {
+    // know cy.visit can also take an obj to specify the url and query string (qs)
+    cy.visit({ url: '/pokemon-search', qs: { name: 'char' } });
+    cy.wait('@api').its('request.url').should('contain', 'name=char');
+  });
 
-  it('should link to the correct pokémon', () => {});
+  // Dev Note: start copy and paste solutions from completed file
+  it('5- should render the results to the page', () => {
+    // a- intercept response with local on the page mock data - see above for pokemon []
+    // - this stubs an api - no need for api server to be running;
+    // - with .intercept, we are effectively "eating" the response and issuing our own response
+    cy.intercept('/pokemon-search/api?*', { pokemon }).as('stubbed-api');
+    // a2- know you can also test one piece of the mock data and use the fixture: keyword
+    //  - note: this works only if there is a file named bulbasaur.json in the fixtures/ dir
+    // cy.intercept('/pokemon-search/api/1', {fixture: 'bulbasaur'}) // inline fixture or external JSON file
+    cy.get('@search').type('lol');
+    // b- wait for the api call - this ensures the api got called
+    cy.wait('@stubbed-api');
+    // c- assert : good example of checking the length of the mock data
+    cy.get('[data-test="result"]').should('have.length', 3); // tests the search results
+  });
 
-  it('should persist the query parameter in the link to a pokémon', () => {});
+  it('6- should link to the correct pokémon from results', () => {
+    // repeat de intercept to stub the api
+    cy.intercept('/pokemon-search/api?*', { pokemon }).as('stubbed-api');
+    // ui
+    cy.get('@search').type('lol');
+    // wait calls the api with fixture
+    cy.wait('@stubbed-api');
+    // * xtra code: loop thru each result and check it's id
+    cy.get('[data-test="result"] a').each(($el, index) => {
+      const { id } = pokemon[index];
+      expect($el.attr('href')).to.contain('/pokemon-search/' + id);
+    });
+  });
 
-  it('should bring you to the route for the correct pokémon', () => {});
+  // dev note: weird: this one makes no sense as 'lol' not in the search results
+  // also very much the same as #6, so don't troubleshoot
+  it('7- should persist the query parameter in the [results] link to a pokémon', () => {
+    cy.intercept('/pokemon-search/api?*', { pokemon }).as('stubbed-api');
 
-  it('should immediately fetch a pokémon if a query parameter is provided', () => {});
+    cy.get('@search').type('lol');
+    cy.wait('@stubbed-api');
+
+    cy.get('[data-test="result"] a').each(($el) => {
+      expect($el.attr('href')).to.contain('name=lol');
+    });
+  });
+
+  it('8- should bring you to the route for the correct pokémon in the render pane', () => {
+    // 2 step api example: - also api stub for passing in a query param
+    cy.intercept('/pokemon-search/api?*', { pokemon }).as('stubbed-api');
+    cy.intercept('/pokemon-search/api/1', { fixture: 'bulbasaur.json' }).as('individual-api');
+
+    cy.get('@search').type('bulba');
+    // api call #1
+    cy.wait('@stubbed-api');
+
+    cy.get('[data-test="result"] a').first().click();
+    // api call #2
+    cy.wait('@individual-api');
+
+    cy.location('pathname').should('contain', '/pokemon-search/1');
+  });
+
+  it.only('9- should immediately fetch a pokémon if a url query parameter is provided', () => {
+    cy.intercept('/pokemon-search/api?*', { pokemon }).as('stubbed-api');
+
+    // how logic? - check the cy.location('pathmame') - nope
+    // using the qs for query-string property
+    cy.visit({ url: '/pokemon-search', qs: { name: 'bulba' } });
+    // nice check of the [mock] response object -- vs always the request object
+    cy.wait('@stubbed-api').its('response.url').should('contain', '?name=bulba');
+  });
 });
--- cypress/integration/11c-pokemon-search-completed.spec.js
+++ cypress/integration/11c-pokemon-search-completed.spec.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 
+// quick & dirty JS obj for the fixture data
 const pokemon = [
   { id: 1, name: 'Bumblesaur' },
   { id: 2, name: 'Charmer' },
@@ -9,47 +10,47 @@ const pokemon = [
 describe('Pokémon Search', () => {
   beforeEach(() => {
     cy.visit('/pokemon-search');
-
+    // aliases
     cy.get('[data-test="search"]').as('search');
     cy.get('[data-test="search-label"]').as('label');
-
     cy.intercept('/pokemon-search/api?*').as('api');
   });
 
-  it('should call the API when the user types', () => {
+  it('1- should call the API when the user types', () => {
     cy.get('@search').type('bulba');
     cy.wait('@api');
   });
 
-  it('should update the query parameter', () => {
+  it('2- should update the query parameter', () => {
     cy.get('@search').type('squir');
     cy.wait('@api');
     cy.location('search').should('equal', '?name=squir');
   });
 
-  it('should call the API with correct query parameter', () => {
+  it('3- should call the API with correct query parameter', () => {
     cy.get('@search').type('char');
     cy.wait('@api').then((interception) => {
       expect(interception.request.url).to.contain('name=char');
     });
   });
 
-  it('should pre-populate the search field with the query parameter', () => {
+  it('4- should pre-populate the search field with the query parameter', () => {
     cy.visit({ url: '/pokemon-search', qs: { name: 'char' } });
     cy.get('@search').should('have.value', 'char');
   });
 
-  it('should render the results to the page', () => {
+  it('5- should render the results to the page', () => {
+    // intercept and provide local mock data
     cy.intercept('/pokemon-search/api?*', { pokemon }).as('stubbed-api');
-
+    // interact with the UI via the DOM
     cy.get('@search').type('lol');
-
+    // async api call
     cy.wait('@stubbed-api');
-
+    // test ui results where api was called and returned data
     cy.get('[data-test="result"]').should('have.length', 3);
   });
 
-  it('should link to the correct pokémon', () => {
+  it('6- should link to the correct pokémon', () => {
     cy.intercept('/pokemon-search/api?*', { pokemon }).as('stubbed-api');
 
     cy.get('@search').type('lol');
@@ -61,9 +62,10 @@ describe('Pokémon Search', () => {
     });
   });
 
-  it('should persist the query parameter in the link to a pokémon', () => {
+  it('7- should persist the query parameter in the link to a pokémon', () => {
     cy.intercept('/pokemon-search/api?*', { pokemon }).as('stubbed-api');
 
+    // error - says field is disabled
     cy.get('@search').type('lol');
     cy.wait('@stubbed-api');
 
@@ -72,7 +74,9 @@ describe('Pokémon Search', () => {
     });
   });
 
-  it('should bring you to the route for the correct pokémon', () => {
+  // 2 step api example: - also api stub for passing in a query param
+
+  it('8- should bring you to the route for the correct pokémon', () => {
     cy.intercept('/pokemon-search/api?*', { pokemon }).as('stubbed-api');
     cy.intercept('/pokemon-search/api/1', { fixture: 'bulbasaur.json' }).as('individual-api');
 
@@ -85,7 +89,7 @@ describe('Pokémon Search', () => {
     cy.location('pathname').should('contain', '/pokemon-search/1');
   });
 
-  it('should immediately fetch a pokémon if a query parameter is provided', () => {
+  it('9- should immediately fetch a pokémon if a query parameter is provided', () => {
     cy.intercept('/pokemon-search/api?*', { pokemon }).as('stubbed-api');
     cy.visit({ url: '/pokemon-search', qs: { name: 'bulba' } });
 
